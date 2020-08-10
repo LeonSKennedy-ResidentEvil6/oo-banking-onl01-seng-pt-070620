21437008ec12b78b97947ad5a9a427612181d99c
Ebo Lee <yiboli.coding@gmail.com>
2020-08-10 19:15:03 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+    valid?(sender, receiver, amount)
+  end
+
+  def valid?(sender,receiver,amount)
+    if self.sender != "" && self.receiver != "" && self.amount > 0
+      true 
+    else 
+      puts "invalid"
+    end 
+  end
+
+
 end
