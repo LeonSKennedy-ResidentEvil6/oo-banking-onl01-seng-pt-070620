ceb96f4e7696bfb63f2577dc4758c73201628faf
Ebo Lee <yiboli.coding@gmail.com>
2020-08-11 19:56:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,14 +24,10 @@ class Transfer
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       self.status = "complete"
-    end
-
-    if not (sender.valid? && receiver.valid?)
+    else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
-
-
   end
 
   def reverse_transfer
